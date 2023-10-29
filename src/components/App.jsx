8602385268a9d1c2769581eb70aff1cd8fcea9a4
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-10-29 23:00:03 +0200

    logic in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,78 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Modal } from './Modal/Modal';
+import { Button } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
+import { getImages } from 'helpers/PixabayAPI';
 
 export class App extends Component {
   state = {
+    page: 1,
     query: '',
+    images: [],
+    isLoading: false,
+    isLoadMore: false,
     showModal: false,
+    error: null,
     modalImage: null,
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const { query } = this.state;
+    if (prevState.query !== query) {
+      try {
+        this.setState({
+          images: [],
+          isLoading: true,
+          isLoadMore: false,
+          page: 1,
+        });
+        const response = await getImages(query, 1);
+        if (response.data.total === 0) {
+          return toast.error('Nothing found for your request');
+        }
+        toast.info(`Hooray. We found ${response.data.totalHits} images`);
+        const totalPages = response.data.totalHits / response.data.hits.length;
+        if (totalPages > 1) {
+          this.setState(({ page }) => {
+            return { isLoadMore: true, page: page + 1 };
+          });
+        }
+        this.setState({ images: response.data.hits });
+      } catch (error) {
+        this.setState({ error });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
+  loadMore = async () => {
+    const { query, images, page } = this.state;
+    try {
+      this.setState({
+        isLoading: true,
+      });
+      const response = await getImages(query, page);
+      this.setState(({ images, page }) => {
+        return {
+          images: [...images, ...response.data.hits],
+          page: page + 1,
+        };
+      });
+      const loadedImages = images.length + response.data.hits.length;
+      if (loadedImages >= response.data.totalHits) {
+        this.setState({ isLoadMore: false });
+        toast.info('You viewed all pictures');
+      }
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
   };
 
   saveQuery = query => {
@@ -24,12 +87,15 @@ export class App extends Component {
   };
 
   render() {
-    const { query, showModal, modalImage } = this.state;
+    const { showModal, modalImage, images, isLoading, isLoadMore, error } =
+      this.state;
     return (
       <div className="container">
         <Searchbar onGetImages={this.saveQuery} />
-
-        <ImageGallery saveQuery={query} openModal={this.toggleModal} />
+        <ImageGallery images={images} openModal={this.toggleModal} />
+        {error && toast.error(`${error.message}`)}
+        {isLoading && <Loader />}
+        {!isLoading && isLoadMore && <Button onButtonClick={this.loadMore} />}
         {showModal && (
           <Modal onCloseModal={this.toggleModal}>
             <img src={modalImage} alt="large" />
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,102 +1,24 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Gallery } from './ImageGallery.styled';
-import { toast } from 'react-toastify';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Button } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
-import { getImages } from 'helpers/PixabayAPI';
 
-export class ImageGallery extends Component {
-  state = {
-    page: 1,
-    images: [],
-    isLoading: false,
-    error: null,
-    isLoadMore: false,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.saveQuery !== this.props.saveQuery) {
-      try {
-        this.setState({
-          images: [],
-          isLoading: true,
-          isLoadMore: false,
-          page: 1,
-        });
-        const response = await getImages(this.props.saveQuery, 1);
-        if (response.data.total === 0) {
-          return toast.error('Nothing found for your request');
-        }
-        toast.info(`Hooray. We found ${response.data.totalHits} images`);
-        const totalPages = response.data.totalHits / response.data.hits.length;
-        if (totalPages > 1) {
-          this.setState(({ page }) => {
-            return { isLoadMore: true, page: page + 1 };
-          });
-        }
-        this.setState({ images: response.data.hits });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
-
-  loadMore = async () => {
-    try {
-      this.setState({
-        isLoading: true,
-      });
-      const response = await getImages(this.props.saveQuery, this.state.page);
-      this.setState(({ images, page }) => {
-        return {
-          images: [...images, ...response.data.hits],
-          page: page + 1,
-        };
-      });
-      const loadedImages = this.state.images.length + response.data.hits.length;
-      if (loadedImages >= response.data.totalHits) {
-        this.setState({ isLoadMore: false });
-        toast.info('You viewed all pictures');
-      }
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  render() {
-    const { images, isLoading, isLoadMore, error } = this.state;
-    return (
-      <>
-        {error && toast.error(`${error.message}`)}
-        {images.length > 0 ? (
-          <Gallery>
-            {images.map(({ id, webformatURL, largeImageURL, tags }) => {
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  smallImg={webformatURL}
-                  showModal={this.props.openModal}
-                  largeImg={largeImageURL}
-                  tags={tags}
-                />
-              );
-            })}
-          </Gallery>
-        ) : null}
-        {isLoading && <Loader />}
-        {!isLoading && isLoadMore && <Button onButtonClick={this.loadMore} />}
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ images, openModal }) => (
+  <Gallery>
+    {images.map(({ id, webformatURL, largeImageURL, tags }) => {
+      return (
+        <ImageGalleryItem
+          key={id}
+          smallImg={webformatURL}
+          showModal={openModal}
+          largeImg={largeImageURL}
+          tags={tags}
+        />
+      );
+    })}
+  </Gallery>
+);
 
 ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.shape),
   openModal: PropTypes.func.isRequired,
-  saveQuery: PropTypes.string.isRequired,
 };
